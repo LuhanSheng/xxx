698d143d8cc454b623f9d082bed37fefaaf424b4
Neo <36170565@my.swjtu.edu.cn>
2019-11-24 15:50:02 +0800

    提交代码

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,29 +6,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-
 public class Main {
-
     public static void main(String[] args) throws Exception {
         JDBCConnectionUtil db= new JDBCConnectionUtil();
-
         //准备用户数据
-        String username = "xxxx";
-        String password= "xxxxx";
-        String studentname= "LuhanSheng";
-        String subject="math";
+        String username = "xxxx";String password= "xxxxx";String studentname= "LuhanSheng";String subject="math";
         User user=new User(username,password,studentname,subject);
-
-        //数据库链接
-        Connection con=db.getConnection();
-
+        Connection con=db.getConnection();        //数据库链接
         String sql="insert into student(userName,password,studentname,subject)values(?,?,?,?)";
         String sql1 = "delete from student where studentname='" + studentname + "'";
         String sql2 = "update student set password='" + password + "' where Name='" + username + "'";
         String sql3 = "select * from student";
-
         PreparedStatement p = null;
-
         p = con.prepareStatement(sql3);
 //        p.setString(1, user.getUserName());
 //        p.setString(2, user.getPassword());
@@ -41,8 +30,7 @@ public class Main {
             String student_password= resultSet.getString("password");
             String student_name = resultSet.getString("studentname");
             String student_subject = resultSet.getString("subject");
-            //输出数据
-            System.out.print("账号："+student_username+" ");
+            System.out.print("账号："+student_username+" ");//输出数据
             System.out.print("密码："+student_password+" ");
             System.out.print("姓名："+student_name+" ");
             System.out.print("课程："+student_subject);
